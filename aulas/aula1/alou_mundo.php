974c762ab1e38d1c6b32d623438e4761fd791a6c
wagnermarques <wagnerdocri@gmail.com>
2015-08-09 21:46:43 -0300

    ola mundo acho que agora he final mesmo

--- aulas/aula1/alou_mundo.php
+++ aulas/aula1/alou_mundo.php
@@ -1,17 +1,58 @@
 <!DOCTYPE html>
 <html>
-<body>
-  <?php
-       echo "Olá mundo! <br>";
-       echo " Olá pessoal de tpi2! <br>";
+  <body>
+    <h1>exercicio 1</h1>
+    <h2>Fun&ccedil;&atilde;o echo</h2>
+    <p></p> 
+    <p>Utilizar a fun&ccedil;&atilde;o echo print para escrever na p&aacute;gina.</p>
+    <p>Pode escrever o que voc&ecirc;s quizerem.</p>
+    <p></p>
+    <p></p> 
+    <a href="https://github.com/wagnermarques/eteczl-tpi2/blob/master/aulas/aula1/alou_mundo.php">codigo
+      deste exercicio no 
+       github</a>
+    <p></p>
+    <a href="http://pastebin.com/vXVYRi4v">codigo
+      deste exercicio no 
+       pastebin</a>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p>Resultado do codigo php segue abaixo:</p>
+    <p></p> 
+    <?php     
+       echo "echo \"ol&aacute; mundo\";";
+       echo "<br>";
+       echo " Ol&aacute; mundo!";
+       echo "<br>";
+       echo "<br>";
+       echo "<br>";
+       echo "<br>";
+       echo "lembre-se de que &eacute; poss&iacute;vel usar a funcao echo tambem com parenteses...";   
+       echo "<br>";
+       echo "<br>";
+       echo ("echo(string);<br>");
+       echo ("string");
+       echo "<br>";
+       echo "<br>";
+       print("<br>");
+       print("<br>");
+       print("<br>");       
+       print("da pra usar a funcao print também<br>");
+       print("print(abc);<br>");
+       print("abc");
+       print("<br>");
+       print("<br>");
+       echo "mais algumas observações...<br>";
+       echo " Olá pessoal de tpi2!<br>";
        echo " Esse arquivo, que está no servidor tem a extensão .php <p>";
-       echo " o nome do arquivo he <strong>alou_mundo.php</strong> <p>";
-       echo " perceba tambem que a url é do navegador é: <p>";
-       echo "
-             http://muc.web44.net/aulas/aula1/<strong>alou_mundo.php</strong> <p>";
-     echo ("echo(string)")
-
-     print("abc");
+       echo " O nome do arquivo &eacute; <strong>alou_mundo.php</strong> <p>";
+       echo " percebam tamb&eacute; que a url do navegador é: <p>";
+       echo "pressione f12 e veja o conteudo html que o seu nevegador
+             recebeu para ser mostrado<br>";
+       echo "constate que o seu navegador no recebeu codigo php...";
+       "http://muc.web44.net/aulas/aula1/<strong>alou_mundo.php</strong> <p>";
+    
     ?>
 </body>
 </html> 
