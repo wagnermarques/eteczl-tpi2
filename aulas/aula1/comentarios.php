9fb568d09ef5720de5839df0770d0dfecda92355
wagnermarques <wagnerdocri@gmail.com>
2015-08-09 22:54:40 -0300

    exercicio comentarios em php

--- aulas/aula1/comentarios.php
+++ aulas/aula1/comentarios.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
-        <?php
+  <h1>Coment&aacute;rios no c&oacute;digo<h1>
+      <p></p>
+      <p></p>
+      <a href="https://github.com/wagnermarques/eteczl-tpi2/blob/master/aulas/aula1/comentarios.php">codigo no github</a>
+      <p></p>
+      <a href="http://pastebin.com/pspCJqAR">codigo no pastebin</a>
+      <p></p>
+      <p>O resultado do codigo php segue abaixo:</p>
+            <p>Esta em branco justmente porque nao tinha o que ser
+              executado, todo o codigo era na verdade comentarios..</p>
+                  <p>pra compreender isso, utilize o link dos codigo e
+                  de uma olhada na codificacao...</p>
+      
+  <?php
                 // Comentario de linha única
                 # Outro comentario de linha única
                 /*
@@ -10,6 +23,5 @@
                 com várias linhas
                 */
         ?>
-        <p>Voc&ecirc; nao vai ver nada porque o codigo php é de coment&aacute;rios</p>
 </body>
 </html> 
